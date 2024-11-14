f9abfe9ba5ccc0a09536daa578ac5e856ae83982
Israel Queiroz <israel.qoliveira@gmail.com>
2024-11-13 23:59:39 -0300

    implementando DTO, repository, service e controller da entidade

---
+++ src/main/java/controllers/GameController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import dto.GameMinDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import services.GameService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+      List<GameMinDTO> result = gameService.findAll();
+      return result;
+    }
+}
---
+++ src/main/java/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
