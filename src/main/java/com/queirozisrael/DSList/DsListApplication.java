cca4d93c401261812087eac4a6b86197be5f876b
Israel Queiroz <israel.qoliveira@gmail.com>
2024-11-13 22:48:06 -0300

    game seed

--- src/main/java/com/queirozisrael/DSList/DsListApplication.java
+++ src/main/java/com/queirozisrael/DSList/DsListApplication.java
@@ -2,8 +2,12 @@ package com.queirozisrael.DSList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("entities")
+@EnableJpaRepositories("com.queirozisrael.DSList")
 public class DsListApplication {
 
 	public static void main(String[] args) {
--- src/main/java/entities/Game.java
+++ src/main/java/entities/Game.java
@@ -1,18 +1,18 @@
 package entities;
 
 import jakarta.persistence.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_game")
+@Table(name ="tb_game")
 public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
     private String title;
 
     @Column(name = "game_year")
@@ -20,24 +20,26 @@ public class Game {
 
     private String genre;
 
-    private String platform;
+    private String platforms;
 
     private Double score;
 
     private String imgUrl;
 
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
 
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -76,12 +78,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
