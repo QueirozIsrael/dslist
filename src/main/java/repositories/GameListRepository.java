891829abac5647a5a0326ceedc3ee6c22dd8b118
Israel Queiroz <israel.qoliveira@gmail.com>
2024-11-15 13:14:28 -0300

    metodo de busca por listas

---
+++ src/main/java/repositories/GameListRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
