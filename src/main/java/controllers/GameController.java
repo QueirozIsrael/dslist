c4f10191dae73d9b9c1c08e5a5e17730d978672c
Israel Queiroz <israel.qoliveira@gmail.com>
2024-11-15 13:05:41 -0300

    implementando o metodo de busca por id

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import dto.GameDTO;
 import dto.GameMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import services.GameService;
@@ -21,4 +23,10 @@ public class GameController {
       List<GameMinDTO> result = gameService.findAll();
       return result;
     }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
+
 }
--- src/main/java/services/GameService.java
+++ src/main/java/services/GameService.java
@@ -1,9 +1,12 @@
 package services;
 
+import dto.GameDTO;
 import dto.GameMinDTO;
 import entities.Game;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import repositories.GameRepository;
 
 import java.util.List;
@@ -14,8 +17,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> result = gameRepository.findAll();
        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
