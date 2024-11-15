1bad95988307a643cc487d7c9d04b5fb6d074050
Israel Queiroz <israel.qoliveira@gmail.com>
2024-11-14 23:22:11 -0300

    ajustes na conexao com o banco de dados

--- src/main/java/com/queirozisrael/DSList/DsListApplication.java
+++ src/main/java/com/queirozisrael/DSList/DsListApplication.java
@@ -3,11 +3,14 @@ package com.queirozisrael.DSList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.queirozisrael.DSList", "controllers", "services", "repositories"})
 @EntityScan("entities")
-@EnableJpaRepositories("com.queirozisrael.DSList")
+@EnableJpaRepositories("repositories")
+
 public class DsListApplication {
 
 	public static void main(String[] args) {
